1497aa58e13ee83c6a2ad1010b4559e27ee78170
Grahulprdxn <rahulg@prdxn.com>
2023-07-09 11:14:49 +0530

    registered model to admin file and created details and listing page

--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-data = {
-    'movies': [
-        {
-            'id': 5,
-            'title': 'Ajooba',
-            'year': '1969'
-        },
-        {
-            'id': 6,
-            'title': 'DDLJ',
-            'year': '1991'
-        },
-        {
-            'id': 7,
-            'title': 'ZMND',
-            'year': '2013'
-        }
-    ]
-}
+from .models import Movie
 
 def movies(request):
-    return render(request, 'movies/movies.html', data)
+    data = Movie.objects.all()
+    return render(request, 'movies/movies.html', {'movies': data})
 
 def home(request):
     return HttpResponse("this is Home Page")
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'detail': data})
